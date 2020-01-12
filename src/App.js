1fd4ee5a13810ebe714f5598797bb8b7d17bfa75
AtulKaushik-hub <atulkau99@gmail.com>
2020-01-12 13:54:52 +0530

    cryptocoin

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import DataSearch from './Components/DataSearch'
+
+ class App extends Component{
+
+    render()
+    {
+        return(
+        
+            <div className = "App">
+            <DataSearch />
+
+            </div>
+        );
+    }
+}
+
+export default App;
